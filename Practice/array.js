76dd9c12236f7af365e23e85304c5196be0156d8
Bustedgut29 <gezansingh@gmail.com>
2023-10-11 15:29:37 +0530

    else if & for loop

---
+++ Practice/array.js
@@ -0,0 +1,9 @@
+const graphic_card = [];
+graphic_card[0] = "rtx 2050";
+graphic_card[1] = "rtx 3050";
+graphic_card[2] = "rtx 4050";
+graphic_card[3] = "rtx 4090";
+console.log(graphic_card[0]);
+console.log(graphic_card[1]);
+console.log(graphic_card[2]);
+console.log(graphic_card[3]);
---
+++ Practice/if_else.js
@@ -0,0 +1,15 @@
+let x=5;
+let i;
+for(i=0; i<=5; i++)
+{
+    if(x>=10)
+    {
+        console.log("hello");
+        
+    }
+    else if(x<=10)
+    {
+        console.log("bye");
+        x++;
+    }
+}
